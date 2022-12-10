2e48d64440db9fa85a6924733cfba8db397dae89
RayaneS <78797911+195048@users.noreply.github.com>
2022-12-10 11:20:51 +0100

    Migration of TeacherController and StudentController to a service layer.

--- src/main/java/GradeManagement/GradeManagement/controller/StudentController.java
+++ src/main/java/GradeManagement/GradeManagement/controller/StudentController.java
@@ -1,6 +1,5 @@
 package GradeManagement.GradeManagement.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,74 +13,46 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import GradeManagement.GradeManagement.exception.ResourceNotFoundException;
 import GradeManagement.GradeManagement.model.Student;
-import GradeManagement.GradeManagement.repository.StudentRepository;
+import GradeManagement.GradeManagement.service.StudentService;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class StudentController {
-
     @Autowired
-    StudentRepository studentRepository;
+    StudentService studentService;
 
     @GetMapping("/students")
-    public ResponseEntity<List<Student>> getAllStudents(@RequestParam(required = false) String name) {
-        List<Student> students = new ArrayList<Student>();
-
-        if (name == null)
-            studentRepository.findAll().forEach(students::add);
-        else
-            studentRepository.findByNameContaining(name).forEach(students::add);
-
-        if (students.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-
-        return new ResponseEntity<>(students, HttpStatus.OK);
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return studentService.getAllStudents();
     }
 
     @GetMapping("/students/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable("id") long id) {
-        Student student = studentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
-
-        return new ResponseEntity<>(student, HttpStatus.OK);
+        return studentService.getStudentById(id);
     }
 
     @PostMapping("/students")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
-        Student _student = studentRepository
-                .save(new Student(student.getName()));
-        return new ResponseEntity<>(_student, HttpStatus.CREATED);
+        return studentService.createStudent(student);
     }
 
     @PutMapping("/students/{id}")
     public ResponseEntity<Student> updateStudent(@PathVariable("id") long id, @RequestBody Student student) {
-        Student _student = studentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found student with id = " + id));
-
-        _student.setName(student.getName());
-
-        return new ResponseEntity<>(studentRepository.save(_student), HttpStatus.OK);
+        return studentService.updateStudent(id,student);
     }
 
     @DeleteMapping("/students/{id}")
     public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") long id) {
-        studentRepository.deleteById(id);
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return studentService.deleteStudent(id);
     }
 
     @DeleteMapping("/students")
     public ResponseEntity<HttpStatus> deleteAllStudents() {
-        studentRepository.deleteAll();
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return studentService.deleteAllStudents();
     }
 
 }
--- src/main/java/GradeManagement/GradeManagement/service/StudentService.java
+++ src/main/java/GradeManagement/GradeManagement/service/StudentService.java
@@ -1,5 +1,68 @@
 package GradeManagement.GradeManagement.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import GradeManagement.GradeManagement.exception.ResourceNotFoundException;
+import GradeManagement.GradeManagement.model.Student;
+import GradeManagement.GradeManagement.repository.StudentRepository;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = new ArrayList<Student>();
+        studentRepository.findAll().forEach(students::add);
+
+        if (students.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") long id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student _student = studentRepository
+                .save(new Student(student.getName()));
+        return new ResponseEntity<>(_student, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Student> updateStudent(@PathVariable("id") long id, @RequestBody Student student) {
+        Student _student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found student with id = " + id));
+
+        _student.setName(student.getName());
+
+        return new ResponseEntity<>(studentRepository.save(_student), HttpStatus.OK);
+    }
+
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") long id) {
+        studentRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    public ResponseEntity<HttpStatus> deleteAllStudents() {
+        studentRepository.deleteAll();
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
     
 }
