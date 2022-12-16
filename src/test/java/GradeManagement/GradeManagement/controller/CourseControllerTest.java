3642f01b1226649400cad5bc07d6f3bd943329b6
Eliott Nys <eliott.nys@gmail.com>
2022-12-16 10:21:33 +0100

    added test for getAllCourses

---
+++ src/test/java/GradeManagement/GradeManagement/controller/CourseControllerTest.java
@@ -0,0 +1,76 @@
+package GradeManagement.GradeManagement.controller;
+
+import GradeManagement.GradeManagement.model.Course;
+import GradeManagement.GradeManagement.repository.CourseRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class CourseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @BeforeEach
+    void setup() { courseRepository.deleteAll(); }
+
+    @Test
+    void getAllCourses() throws Exception {
+        // given
+        List<Course> listOfCourses = new ArrayList<>();
+        listOfCourses.add(new Course("Software Architecture"));
+        listOfCourses.add(new Course("Software Architecture and Quality Lab"));
+        courseRepository.saveAll(listOfCourses);
+
+        // when
+        ResultActions response = mockMvc.perform(get("/api/courses"));
+
+        // then
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()",
+                        is(listOfCourses.size())));
+    }
+
+    @Test
+    void getCourseById() {
+    }
+
+    @Test
+    void createCourse() {
+    }
+
+    @Test
+    void updateCourse() {
+    }
+
+    @Test
+    void assignTeacherToCourse() {
+    }
+
+    @Test
+    void deleteCourse() {
+    }
+
+    @Test
+    void deleteAllCourses() {
+    }
+}
